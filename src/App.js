d5876e3d3b7f421ac8de33ce195bb28a21e656df
san <hillsfrontendweb@gmail.com>
2020-05-07 20:12:31 -0400

    props children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <CardList name = "Yihua"/>
         {
           this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
         }
