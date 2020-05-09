d3d477eb64c39f7707bb7e6d9dd6e410565c7fcd
san <hillsfrontendweb@gmail.com>
2020-05-08 23:35:40 -0400

    search field

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
   
@@ -21,6 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <input type='search' placeholder='search monsters' onChange={ e => console.log(e.target) } />
       <CardList monsters={this.state.monsters}>
         
       </CardList>
